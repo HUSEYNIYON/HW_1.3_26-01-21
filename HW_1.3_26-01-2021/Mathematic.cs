73adaf38eea2c673598bfafeba8154adbf079184
Huseyn <huseyn19.burhonov1999@gmail.com>
2021-01-30 19:40:47 +0500

    Добавьте файлы проекта.

---
+++ HW_1.3_26-01-2021/Mathematic.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_1._3_26_01_2021
+{
+    class Mathematic
+    {
+        public int N { get; set; }
+        public string text { get; set; }
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public Mathematic(int n)
+        {
+            N = n;
+        }
+        public Mathematic(string text)
+        {
+            this.text = text;
+        }
+        public Mathematic(int num1, int num2, int v)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+
+        public Mathematic(int n, int num2) : this(n)
+        {
+            this.num2 = num2;
+        }
+
+        public void getMulTab()
+        {
+            Console.WriteLine("Таблица умножение");
+            for (int j = 1; j <= N; j++)
+            {
+                for (int i = 1; i <= N; i++)
+                {
+                    Console.Write("{0}*{1}={2}\t", i, j, (i * j));
+                }
+                Console.WriteLine();
+            }
+        }
+        public void getPolindrom()
+        {
+            int len = text.Length;
+            bool flag = true;
+
+            //Проверка полиндрома
+            for (int i = 0; i < len / 2; i++)
+            {
+                if (text[i] != text[len - (i + 1)])
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+
+            if (flag)
+            {
+                Console.WriteLine("{0} Полиндром!", text);
+            }
+            else
+            {
+                Console.WriteLine("{0} Не полиндром!", text);
+            }
+            Console.ReadLine();
+        }
+        public void checkEven()
+        {
+            if (N % 2 == 0)
+            {
+                Console.WriteLine("{0} четный", N);
+            }
+            else
+            {
+                Console.WriteLine("{0} не четный", N);
+            }
+        }
+        public void simpleNum()
+        {
+            int sayac = 0;
+            Console.WriteLine("Простые число в диапозоне {0} и {1} : ", num1, num2);
+            Console.WriteLine("==============================================");
+            for (int i = num1; i < num2; i++)
+            {
+                sayac = 0;
+                if (i > 1)
+                {
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            sayac = 1;
+                            break;
+                        }
+                    }
+                    if (sayac == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+        public void sumNum()
+        {
+            int sum = 0;
+            if (N < 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Пж. Введите положительное число!");
+            }
+            else
+            {
+                while (N > 0)
+                {
+                    sum += N;
+                    N -= 1;
+                }
+            }
+            Console.WriteLine("Сумма = " + sum);
+
+        }
+    }
+    class Geometric
+    {
+        private double firstSide { get; set; }
+        private double secondSide { get; set; }
+        public Geometric(double FirstSide, double SecondSide)
+        {
+            firstSide = FirstSide;
+            secondSide = SecondSide;
+        }
+        public double solveGeoArea()
+        {
+            return firstSide * secondSide;
+           
+        }
+        public double solveGeoPerim()
+        {
+            return (2 * firstSide) + (2 * secondSide);
+        }
+        public double getMidGeo()
+        {
+            return Math.Sqrt(firstSide * secondSide);
+        }
+
+    }
+    class FizRa
+    {
+        public int firstValue { get; set; }
+        public int secondValue { get; set; }
+        public FizRa(int fv, int sv)
+        {
+            firstValue = fv;
+            secondValue = sv;
+        }
+        public void extraWeight()
+        {
+            
+        }
+    }
+}
---
+++ HW_1.3_26-01-2021/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace HW_1._3_26_01_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            lll:
+            Console.WriteLine("Это программа преднозначано для школьников и а также для студентов!");
+            Console.WriteLine(@"Выбирите один вариант из нижеперечисленных:
+                           1. Математика --> 1
+                           2. Геометрия  --> 2
+                           3. Физр-ра   --> 3");
+            var check = int.TryParse(Console.ReadLine(), out var myNumber);
+            if (check)
+            {
+                if(myNumber>0 && myNumber < 4)
+                {
+                    switch (myNumber)
+                    {
+                        case 1:
+                            Console.WriteLine(@"Выбирите операцию:
+                                    1. Таблица умножения --> 1
+                                    2. Полиндром --> 2
+                                    3. Проверка числы на Четность --> 3
+                                    4. Простые число на интервале а и b --> 4
+                                    5. Сумма N число --> 5 ");
+                            Console.WriteLine("Ваш выбор: ");
+                            var checkMath = int.TryParse(Console.ReadLine(), out var myNumberMath);
+                            if (myNumber > 0 && myNumber < 3)
+                            {
+                                switch (myNumberMath)
+                                {
+                                    case 1:
+                                        Console.WriteLine("Введите степень: ");
+                                        int pow = Convert.ToInt32(Console.ReadLine());
+                                        Mathematic math = new Mathematic(pow);
+                                        math.getMulTab();
+                                        break;
+                                    case 2:
+                                        Console.Write("Введите текст для проверки :");
+                                        string text = Console.ReadLine();
+                                        var math1 = new Mathematic(text);
+                                        math1.getPolindrom();
+                                        break;
+                                    case 3:
+                                        Console.Write("Введите число : ");
+                                        int n = Int32.Parse(Console.ReadLine());
+                                        var math3 = new Mathematic(n);
+                                        math3.checkEven();
+                                        break;
+                                    case 4:
+                                        int num1, num2;
+                                        Console.Write("Введите первый диапозон: ");
+                                        num1 = Convert.ToInt32(Console.ReadLine());
+                                        Console.Write("Введите второй диапозон: ");
+                                        num2 = Convert.ToInt32(Console.ReadLine());
+                                        var math4 = new Mathematic(num1, num2);
+                                        math4.simpleNum();
+                                        break;
+                                    case 5:
+                                        int number;
+                                        Console.Write("Введите N : ");
+                                        number = Convert.ToInt32(Console.ReadLine());
+                                        var math5 = new Mathematic(number);
+                                        math5.sumNum();
+                                        break;
+                                    default:
+                                        Console.ForegroundColor = ConsoleColor.Red;
+                                        Console.WriteLine("Вы выбрали неправылную команду!");
+                                        break;
+                                }
+                            }
+                                break;
+                        case 2:
+                            Geometric geometric;
+                            Console.WriteLine(@"Выбирите операцию:
+                                    1. Площадь прямоугольника --> 1
+                                    2. Периметр прямоугольника --> 2
+                                    3. Средное арифметическое --> 3");
+                            Console.WriteLine("Ваш выбор: ");
+                            var checkGeo = int.TryParse(Console.ReadLine(), out var myNumberGeo);
+                            Console.Write($"firstSide = ");
+                            double firstSide = double.Parse(Console.ReadLine());
+                            Console.Write($"firstSide = ");
+                            double secondSide = double.Parse(Console.ReadLine());
+                            geometric = new Geometric(firstSide, secondSide);
+                            if(myNumber>0 && myNumber < 3)
+                            {
+                                switch (myNumberGeo)
+                                {
+                                    case 1:
+                                        Console.WriteLine($"Area: {firstSide}*{secondSide} = {geometric.solveGeoArea()}");
+                                        break;
+                                    case 2:
+                                        Console.WriteLine($"Perimeter: {firstSide}*{secondSide}={geometric.solveGeoPerim()}");
+                                        break;
+                                    case 3:
+                                        Console.WriteLine($"Cред. геометрическое: {geometric.getMidGeo()}");
+                                        break;
+                                }
+                            }
+                            break;
+                        case 3:
+
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы вводили не число");
+            }
+            goto lll;
+        }
+    }
+}
